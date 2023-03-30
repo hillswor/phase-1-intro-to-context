d6b851ef312637406c692d8317ad68e3b3a823cc
Bruce <bruce.hillsworth@gmail.com>
2023-03-29 19:42:45 -0600

    Added createEmployeeRecord and createEmployeeRecords functions

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Your code here
+function createEmployeeRecord(employeeInfo) {
+	const employee = {
+		firstName: employeeInfo[0],
+		familyName: employeeInfo[1],
+		title: employeeInfo[2],
+		payPerHour: employeeInfo[3],
+		timeInEvents: [],
+		timeOutEvents: [],
+	};
+	return employee;
+}
+
+function createEmployeeRecords(arrayOfArrays) {
+	let employees = [];
+	arrayOfArrays.forEach((array) => {
+		employees.push(createEmployeeRecord(array));
+	});
+	return employees;
+	console.log(employees);
+}
